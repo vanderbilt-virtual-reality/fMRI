427ed7a74657893a0471d719ea31b067e29a88c1
Brandon Born <brandon.born@vanderbilt.edu>
2019-12-09 17:25:19 -0600

    Project files updated

---
+++ fMRI_Environment/Assets/src/Coronal1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Coronal1 : MonoBehaviour
+{
+    public SteamVR_Action_Boolean moveSag1;
+    public SteamVR_Input_Sources handType;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSag1.AddOnStateDownListener(TriggerDown, handType);
+    }
+
+    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is down");
+        player.transform.position = new Vector3(0, 0, -150);
+        player.transform.rotation = transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ fMRI_Environment/Assets/src/Sagittal2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Sagittal2 : MonoBehaviour
+{
+    public SteamVR_Action_Boolean moveSag1;
+    public SteamVR_Input_Sources handType;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSag1.AddOnStateDownListener(TriggerDown, handType);
+    }
+
+    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is down");
+        player.transform.position = new Vector3(150, 0, 0);
+        player.transform.rotation = transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
